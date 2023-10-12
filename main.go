1babf718dba0a90ac43ddf4bca7c513cfb1c8347
outproblem <147035781+outproblem@users.noreply.github.com>
2023-10-12 09:00:45 +0000

    Protokol

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-func main()  {
-	println("Hello")
+import (
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	println("Net Demo server")
+
+	http.HandleFunc("/", indexHandler)
 	
+	http.ListenAndServe("127.0.0.1:9010", nil)
 }
+
+	
+func indexHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Printf("%#v\n", r)
+
+	w.Write([]byte("Hello!"))
+}
+	
